84c154dd5938230d0ada50812888a8cd24f050af
Charles Bloomfield <charlie@wpa147220.wireless.mcgill.ca>
2017-01-23 16:53:00 -0500

    DELETE: dead code

--- src/Language.hs
+++ src/Language.hs
@@ -13,27 +13,3 @@ data Exp
   | String String
   | Id String
   deriving (Show, Eq)
-
--- Parser expressions
--- data Exp
---   = Let String Exp Exp
---   | Exp1 Exp1
---   deriving (Show,Eq)
---
--- data Exp1
---   = Plus Exp1 Term
---   | Minus Exp1 Term
---   | Term Term
---   deriving (Show,Eq)
---
--- data Term
---   = Times Term Factor
---   | Div Term Factor
---   | Factor Factor
---   deriving (Show,Eq)
---
--- data Factor
---   = Int Int
---   | Var String
---   | Brack Exp
---   deriving (Show,Eq)
