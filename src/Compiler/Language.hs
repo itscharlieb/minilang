e6962b37aade5a07aba86296ccad3fd0db66759b
Charles Bloomfield <charlie@wpa146036.wireless.mcgill.ca>
2017-02-02 19:12:28 -0500

    REFORMAT

--- src/Compiler/Language.hs
+++ src/Compiler/Language.hs
@@ -7,7 +7,7 @@ data Program
   deriving (Show, Eq)
 
 
--- Declerations
+-- Declarations
 data Dclr
   = IntId String
   | FloatId String
@@ -41,33 +41,34 @@ data Exp
   deriving (Show, Eq)
 
 
---
+-- |Converts program to a pretty string using \t and \n as whitespace chars
 pretty :: Program -> String
 pretty (Program dclrs stmts) =
   prettyDclrs dclrs ++ "\n" ++ prettyStmts stmts ""
 
 
---
+-- |Converts declaration to a pretty string.
 prettyDclrs :: [Dclr] -> String
 prettyDclrs [] = ""
 prettyDclrs (dclr:dclrs) =
-  (prettyDclr dclr) ++ "\n" ++ (prettyDclrs dclrs)
+  prettyDclr dclr ++ "\n" ++ prettyDclrs dclrs
 
 
---
+-- |Converts declaration to a pretty string.
 prettyDclr :: Dclr -> String
 prettyDclr (IntId name) = "var " ++ name ++ ": int;"
 prettyDclr (FloatId name) = "var " ++ name ++ ": float;"
 prettyDclr (StringId name) = "var " ++ name ++ ": string;"
 
 
---
+-- |Converts statement list to a pretty string.
 prettyStmts :: [Stmt] -> String -> String
 prettyStmts [] _ = ""
 prettyStmts (stmt:stmts) indent =
-  indent ++ (prettyStmt stmt indent) ++ "\n" ++ (prettyStmts stmts indent)
+  indent ++ prettyStmt stmt indent ++ "\n" ++ prettyStmts stmts indent
 
 
+-- |Converts statement to a pretty string.
 prettyStmt :: Stmt -> String -> String
 prettyStmt (Print e) _ = "print " ++ prettyExp e ++ ";"
 prettyStmt (Read name) _ = "read " ++ name ++ ";"
@@ -85,7 +86,7 @@ prettyStmt (IfElse e stmts1 stmts2) indent =
 prettyStmt (Exp e) _ = prettyExp e
 
 
---
+-- |Converts expression to a pretty string.
 prettyExp :: Exp -> String
 prettyExp (Negate e) = "-" ++ prettyExp e
 prettyExp (Plus e1 e2) = prettyBinary e1 e2 "+"
@@ -98,6 +99,7 @@ prettyExp (String s) = s
 prettyExp (Id s) = s
 
 
+-- |Helper function that converts any binary operation to a pretty string.
 prettyBinary :: Exp -> Exp -> String -> String
 prettyBinary e1 e2 op =
-  "(" ++ (prettyExp e1) ++ " " ++ op ++ " " ++ (prettyExp e2) ++ ")"
+  "(" ++ prettyExp e1 ++ " " ++ op ++ " " ++ prettyExp e2 ++ ")"
